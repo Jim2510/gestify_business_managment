e614efa05c8d1f6ceb58898132e2c93f92c2f8cc
Curcetti <felice7curcetti@gmail.com>
2024-04-28 14:53:16 +0200

    make storage

--- gestify/src/components/activities_comp/BaseActivities.jsx
+++ gestify/src/components/activities_comp/BaseActivities.jsx
@@ -5,7 +5,7 @@ export function BaseActivities() {
     return (
         <>
          <div className="w-[100vw] flex justify-center flex-col items-center ">
-            <TitleSection titleName={'ACTIVITIES'} />
+            <TitleSection titleName={'ORDER HISTORY'} />
             <div className="w-[85%] h-[500px] mt-[20px] shadow-2xl rounded-xl flex justify-center">
                <TableActivities />
             </div>
