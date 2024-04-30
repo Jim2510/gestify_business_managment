0b4c85b814fd308a1fb6fa471d43042a90bc7ca8
Jim2510 <gmguglielmino@gmail.com>
2024-04-30 19:08:22 +0200

    Dashboard
    dashboard sales component completed

---
+++ gestify/src/components/dashboard_comp/sales/SingleNews.jsx
@@ -0,0 +1,14 @@
+export function SingleNews({ earns }) {
+  return (
+    <>
+      <div className="w-[90%] h-fit flex justify-end py-2  border-2 border-gray-200 items-center">
+        <div className="w-full flex justify-between px-4">
+          <h3 className="text-center w-[50px] bg-gradient-to-b from-[#1cdafe] to-[#02f9ae]">
+            {earns}
+          </h3>
+          <h3>New transaction</h3>
+        </div>
+      </div>
+    </>
+  );
+}
