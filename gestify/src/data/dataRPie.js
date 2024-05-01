71dd397e3a933bc2f8b76a56528cc239259d658c
Jim2510 <gmguglielmino@gmail.com>
2024-05-01 16:16:23 +0200

    Dashboard expenditures
    expenditure dashboard component restyled

---
+++ gestify/src/data/dataRPie.js
@@ -0,0 +1,34 @@
+const dataRPie = [
+  {
+    id: "python",
+    label: "python",
+    value: 203,
+    color: "hsl(297, 70%, 50%)",
+  },
+  {
+    id: "css",
+    label: "css",
+    value: 133,
+    color: "hsl(75, 70%, 50%)",
+  },
+  {
+    id: "rust",
+    label: "rust",
+    value: 135,
+    color: "hsl(7, 70%, 50%)",
+  },
+  {
+    id: "erlang",
+    label: "erlang",
+    value: 544,
+    color: "hsl(135, 70%, 50%)",
+  },
+  {
+    id: "hack",
+    label: "hack",
+    value: 369,
+    color: "hsl(284, 70%, 50%)",
+  },
+];
+
+export default dataRPie;
