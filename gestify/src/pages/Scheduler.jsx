996d196aad05636682a4f1a2dc89576302fa3363
Gabriele <gabri46nicotra@gmail.com>
2024-05-05 18:28:35 +0200

    added calendar

--- gestify/src/pages/Scheduler.jsx
+++ gestify/src/pages/Scheduler.jsx
@@ -5,7 +5,24 @@ import { SchedulerGrid } from "../components/scheduler_comp/SchedulerGrid";
 export function Scheduler() {
     const [colomuns, setColumns] = useState([])
     const [currentWeek, setCurrentWeek] = useState([]);
+    const [currentDate, setCurrentDate] = useState(getFormattedDate());
+    function getFormattedDate() {
+        const today = new Date();
+        const year = today.getFullYear();
+        let month = today.getMonth() + 1;
+        let day = today.getDate();
 
+        // Aggiungi uno zero davanti al mese e al giorno se sono inferiori a 10
+        month = month < 10 ? '0' + month : month;
+        day = day < 10 ? '0' + day : day;
+
+        return `${year}-${month}-${day}`;
+    }
+
+    // Funzione per gestire il cambiamento della data nell'input
+    function handleDateChange(event) {
+        setCurrentDate(event.target.value);
+    }
     useEffect(() => {
         const currentDate = new Date();
         const startOfCurrentWeek = startOfWeek(currentDate); // Ottieni l'inizio della settimana corrente
@@ -23,13 +40,16 @@ export function Scheduler() {
         '15:00 ', '16:00 ', '17:00 ', '18:00 ', '19:00 ', '20:00 ', '21:00 ', '22:00 ',
         '23:00 ', '0:00 ', '1:00 ', '2:00 ', '3:00 ', '4:00 ', '5:00 ', '6:00 '
     ];
+    function handleDateChange(event) {
+        setCurrentDate(event.target.value);
+    }
     return (
         <div className="flex flex-col w-[100vw] h-[100vh] text-center justify-center items-center mt-[30px] bg-gradient-to-b from-[#1cdafe] to-[#02f9ae]">
             <TitleSection titleName="CALENDAR" />
             <div className="w-[85%] h-[75%] overflow-y-auto">
                 <div className="grid grid-cols-8 grid-rows-26 w-full h-fit shadow-lg rounded-md bg-white relative font-semibold">
                     <div className="col-span-full bg-gray-200 flex justify-between sticky top-0 z-20">
-                        <input type="date" className="border-none bg-transparent"></input>
+                        <input type="date" className="border-none bg-transparent" value={currentDate} onChange={handleDateChange}></input>
                         <div className="flex justify-center items-center">
                             <button className="mr-[30px] border-[2px] border-white rounded-full px-[10px] font-light" >Day</button>
                             <button className="mr-[30px] border-[2px] border-white rounded-full px-[10px] font-light" >Week</button>
