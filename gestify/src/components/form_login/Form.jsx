c3db5a83517606e145dee35aa40f379c02519784
Gabriele <gabri46nicotra@gmail.com>
2024-05-06 09:39:33 +0200

    email input commentato

--- gestify/src/components/form_login/Form.jsx
+++ gestify/src/components/form_login/Form.jsx
@@ -62,13 +62,13 @@ export function Form() {
           </svg>
           <input style={inputStyle.input} className="w-[70%] h-[60%] bg-transparent border-none" onInput={handleInput} name="username" type="text" placeholder="Username" />
         </div>
-        <div className="bg-[#E9EFFF] w-[300px] h-[50px] rounded-3xl shadow-lg mt-[25px] ml-[15px] flex items-center justify-center">
+        {/* <div className="bg-[#E9EFFF] w-[300px] h-[50px] rounded-3xl shadow-lg mt-[25px] ml-[15px] flex items-center justify-center">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="" class="fill-[#1E293B]" viewBox="0 0 16 16">
             <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
           </svg>
           <input style={inputStyle.input} className="w-[70%] h-[60%] bg-transparent border-none" onInput={handleInput}
             value={form.email} type="email" name="email" placeholder="Email" />
-        </div>
+        </div> */}
         <div className="bg-[#E9EFFF] w-[300px] h-[50px] rounded-3xl shadow-lg mt-[25px] ml-[15px] flex items-center justify-center">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="" class="fill-[#1E293B]" viewBox="0 0 16 16">
             <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2" />
